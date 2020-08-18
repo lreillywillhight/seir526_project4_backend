79c734df60bce8dee7508ce689a523135c341a6b
louis <lreillywillhight@gmail.com>
2020-08-18 08:44:37 -0700

    deploy to heroku to fix cors error

---
+++ server.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const express = require('express')
+const { graphqlHTTP } = require('express-graphql')
+const schema = require('./schema/schema')
+const mongoose = require('mongoose')
+// const cors = require('cors')
+
+const app = express()
+
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true
+}))
+
+mongoose.connect(process.env.MONGODB_URI)
+
+mongoose.connection.once('open', () => {
+  console.log('connected to mongodb Atlas')
+})
+
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`server.js listening on port ${process.env.PORT}`)
+})
